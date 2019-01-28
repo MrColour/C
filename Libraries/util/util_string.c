6638efa3bf7697e349d8d758c14ac8a4bfcdc828
Kevin Colour <41839667+MrColour@users.noreply.github.com>
2019-01-28 13:07:43 -0800

    📚🔬💊

--- Libraries/util/util_string.c
+++ Libraries/util/util_string.c
@@ -11,18 +11,20 @@
  *      Installs: mingw-get 0.6.3
  * 
  * VERSION:
- *      0.0.0.2
+ *      0.0.0.3
  * 
  * AUTHOR(s):
  *      Kevin Colour
  *
  * DATES:
- *      Created: 23Dec2018          Verified Execute: 09Jan2019
+ *      Created: 23Dec2018          Verified Execute: 25Jan2019
  *
 ****************************************************************/
 
 #ifndef UTIL_STRING
 # define UTIL_STRING
+
+#include <unistd.h>
 
 int     str_len(char *str)
 {
@@ -40,18 +42,29 @@ int     str_cmp(char *s1, char *s2)
         s1++;
         s2++;
     }
-    return (*s2 - *s1);
+    return (*s1 - *s2);
 }
 
-char    *str_cpy(char *dest, char *src)
+char    *str_cpy_n(char *dest, char *src, int n)
 {
     char *head;
 
     head = dest;
-    while (*src)
-        *dest++ = *src++;
-    *dest = '\0';
+    if (n < 0)
+    {
+        write(1, "Error in value of n.\n METHOD \t\'str_cpy_n\'\n", 42);
+    }
+    while (n-- > 0)
+        if (*src)
+            *dest++ = *src++;
+        else
+            *dest++ = '\0';
     return (head);
+}
+
+char    *str_cpy(char *dest, char *src)
+{
+    return (str_cpy_n(dest, src, str_len(src)));
 }
 
 #endif
